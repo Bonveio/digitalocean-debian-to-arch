c46f927d27fdefbb0dfa106cab92176cfd337c3f
Bonveio Abitona <bonveiochan@gmail.com>
2025-01-05 06:49:48 +0800

    remove dangling tar option to `extract_digitalocean_synchronize()` function

--- install.sh
+++ install.sh
@@ -155,7 +155,7 @@ extract_digitalocean_synchronize() {
 	local outdir="$1"
 	mkdir -p "${outdir}"
 	awk 'x {print} $0 == "### digitalocean-synchronize ###" {x=1}' "$0" | \
-		base64 -d | tar -zxC "${outdir}"
+		base64 -d | tar -xC "${outdir}"
 }
 
 parse_flags() {
